df492aeb44c98650064effec229250a29091d2ba
Bing Han <h.bing612@gmail.com>
2012-09-09 13:50:40 +0800

    Init the project and change erb to slim , and use mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leader::Application.config.secret_token = '08eb0970ca2746171d25787f865ed45bfb13a552d688ddb9fa88bfe810e53132857448c085946c3f38beeaaf71a32c917305588817da8bb32482802acaffb283'
