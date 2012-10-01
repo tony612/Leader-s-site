a0989fadae89951836564e5c8c71a9e5ed44855b
Bing Han <h.bing612@gmail.com>
2012-10-01 14:11:27 +0800

    Change the products and about view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,28 @@
 class StaticPagesController < ApplicationController
   skip_before_filter :signed_in_user
 
-  layout "single", :only => [:about]
+  #layout "single", :only => [:about]
 
   def home
-    @news = News.order_by(:created_at.desc, :title.asc).limit(4)
+    @news = News.order_by(:created_at.desc, :title.asc).limit(5)
     #@news = News.find()
   end
 
   def about
+    @item = params[:item]? params[:item] : 'guanyuwomen'
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def products
+    @item = params[:item]? params[:item] : 'dhl'
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
